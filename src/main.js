55b3a088c2c800ffb68ecd2be234c1e1bb70302f
Look-stars <1579968001@qq.com>
2019-04-30 11:18:03 +0800

    首尾部分完毕

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入全局样式  只导入不使用  可以省略  xxx from
+import './assets/statics/site/css/style.css'
 Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App),
